31b2f03435217ff0d0abc0c06c65522dfed8a2d7
Niclas Titius <niclas.titius@nicti.de>
2024-05-20 18:26:45 +0200

    Bash

--- restart_api.sh
+++ restart_api.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 API_SERVER_ID=$(screen -ls | grep api_server | cut -d. -f1 | awk '{print $1}');
-kill "$API_SERVER_ID"; screen -dmS api_server sh -c 'nvm use 18; yarn start:api'
+kill "$API_SERVER_ID"; screen -dmS api_server bash -c 'nvm use 18; yarn start:api'
