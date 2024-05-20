4a30c701465bdb5b77be8f88af4893cabc224f7d
Niclas Titius <niclas.titius@nicti.de>
2024-05-20 11:34:45 +0200

    json fix

--- packages/api-server/src/index.ts
+++ packages/api-server/src/index.ts
@@ -31,7 +31,7 @@ const app = express()
   // app.use('/api/user_info', User)
 
   app.use(bodyParser.json())
-  app.use(bodyParser.urlencoded({ extended: false }))
+  app.use(express.json())
   app.use(cookieParser())
   app.use(compression())
 
