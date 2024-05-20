636513d5e2fb42c310246a90f85c323327bdc885
Niclas Titius <niclas.titius@nicti.de>
2024-05-20 14:49:12 +0200

    bodyParser to express

--- packages/api-server/src/index.ts
+++ packages/api-server/src/index.ts
@@ -30,7 +30,8 @@ const app = express()
   // })
   // app.use('/api/user_info', User)
 
-  app.use(bodyParser.json())
+  app.use(express.json())
+  app.use(express.urlencoded({ extended: true }))
   app.use(cookieParser())
   app.use(compression())
 
